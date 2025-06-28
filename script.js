24987c11dfa068228506e9f10bffda56e18b239f
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-06-28 12:38:24 +0530

    add foodItem in arena

--- script.js
+++ script.js
@@ -15,19 +15,60 @@ document.addEventListener("DOMContentLoaded", () => {
   let dx = cellSize; // displacement on x axis
   let dy = 0; // displacement on y axis
 
-  function startGame() {
+  function drawScoreBoard() {
+    const scoreBoard = document.getElementById('score-board');
+    scoreBoard.textContent = `Score: ${score}`; 
+  }
+
+  function drawDiv (x, y, className) {
+    const div = document.createElement('div');
+    div.classList.add(className);
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
+    return div;
+  }
+
+  function drawFoodAndSnake() {
+    // if previously something is drawn remove it
+    // wipe out everything and redraw with the new coordinates when snake moves.
+    gameArea.innerHTML = '';
+
+     const foodElement = drawDiv(food.x, food.y, 'food');
+     gameArea.appendChild(foodElement);
+
+
+  }
+
+
+  // EVERY ONE SEC WE HAVE TO CHANGE SOMETHING BY THIS FUNCTION
+  function gameLoop() {
+    setInterval(() => {
+        drawScoreBoard();
+        drawFoodAndSnake();
+    }, 1000);
+  }
+
+  function runGame() {
+    gameStarted = true;
+    gameLoop();
+  }
+
+  function initiateGame() {
     const scoreBoard = document.createElement("div");
     scoreBoard.id = "score-board";
-    scoreBoard.textContent = "10";
+    scoreBoard.textContent = "";
     document.body.insertBefore(scoreBoard, gameArea);
 
-
-
-
-    const startButton = document.createElement('button');
-    startButton.textContent = 'Start Game';
-    startButton.classList.add('start-button');
+    const startButton = document.createElement("button");
+    startButton.textContent = "Start Game";
+    startButton.classList.add("start-button");
     document.body.appendChild(startButton);
+
+    startButton.addEventListener("click", () => {
+      startButton.style.display = "none";
+      runGame();
+    });
   }
-  startGame();
+  // this is the first fnc to be executed so that we have make the UI.
+  initiateGame();
 });
