bd3236b9d70e34e2868f2ac0ce32dfa70cc69957
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-06-28 11:45:09 +0530

    add playing area

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("script working");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gameArea = document.getElementById("game-area");
+    const areaSize = 800;
+    const cellSize = 20;
+    let score = 0;
+    let gameStarted = false;
+    let food = {x:300, y:200};
+    let snake = [{x:160, y:200}, {x:140, y:200}, {x:120, y:200}];
+
+
+
+
+
+
+
+});
